6736545bc283c4b1f304591b9f01e207df122cc0
Kristina Gorina <kristina.gorina@clover.com>
2023-05-18 17:38:21 -0400

    server working

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const tasks = require('./tasks');
 const notes = require('./notes');
 
-router.use('/tasks', tasks);
-router.use('/notes', notes);
+//uncomment later
+
+//router.use('/tasks', tasks);
+//router.use('/notes', notes);
 
 module.exports = router;
