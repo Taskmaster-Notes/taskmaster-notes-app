3d431a128084c17863670ff664f018c4247f44cc
Kristina Gorina <kristina.gorina@clover.com>
2023-05-25 13:01:40 -0400

    create user routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const tasks = require('./tasks');
 const notes = require('./notes');
+const users = require('./users');
 
 //uncomment later
-
+router.use('/users',users);
 //router.use('/tasks', tasks);
 //router.use('/notes', notes);
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -5,14 +5,14 @@ const { User } = require('../models/User');
 //no auth yet
 
 //GET all users
-// router.get("/", async (req, res, next)=>{
-//     if(!await User.findAll()){
-//         res.sendStatus(404)
-//     }
-//     else{
-//         res.json(await User.findAll())
-//     }
-// })
+router.get("/", async (req, res, next)=>{
+    if(!await User.findAll()){
+        res.sendStatus(404)
+    }
+    else{
+        res.json(await User.findAll())
+    }
+})
 //GET one user
 router.get("/:id", async (req, res, next)=>{
     if(!await User.findByPk(req.params.id)){
@@ -22,22 +22,30 @@ router.get("/:id", async (req, res, next)=>{
         res.json(await User.findByPk(req.params.id))
     }
 });
+//POST user
+router.post('/', async (req,res,next)=>{
+        const { firstName, lastName, username, password } = req.body;
+        const newUser = await User.create({
+            firstName,
+            lastName,
+            username,
+            password
+        });
+        res.status(201).send({newUser});
+    })
+//PUT user
+router.put('/:id', async (req,res)=> {
+    const user = await User.findByPk(req.params.id);
+    const { firstName, lastName, username, password } = req.body;
+    await user.update({ firstName, lastName, username, password });
+    res.status(201).send(user);
+})
 //delete -- example
-// router.delete("/:id", async (req, res) => {
-//     const user = await User.findByPk(req.params.id);
-//     await user.destroy();
-//     res.sendStatus(204);
-//   });
-
-//   router.delete('/:id', authenticateJWT, async (req, res, next)=>{
-    
-//         const toDelete = await Recipe.findByPk(req.params.id);
-//         await toDelete.destroy();
-//         console.log(isAdmin)
-//         res.json(toDelete)
-    
-    
-// });
+router.delete("/:id", async (req, res) => {
+    const user = await User.findByPk(req.params.id);
+    await user.destroy();
+    res.sendStatus(204);
+  });
 
 module.exports = router;
   
