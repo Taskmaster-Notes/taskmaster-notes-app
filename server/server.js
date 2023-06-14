f11dffa3b8550682ebc0c62c578e0e8b545a9f69
Annalise Grekalski <grekalski842@gmail.com>
2023-06-14 14:17:49 -0400

    fixing deployment issue

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const init = async () => {
         await sequelize.sync();
 
         app.listen(PORT, ()=> {
-            console.log(`Server listening at http://localhost:${PORT}`);
+            console.log(`Server listening at http://localhost:3000`);
             
         });
     }
